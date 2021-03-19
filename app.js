9b1d9268478c3af2f98f6055574d1d257386bf75
Brittany <bgreensl@ualberta.ca>
2021-03-18 22:58:28 -0600

    added collab statement

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+//Collaborated with Shawn
+
 // The users' ages and their respective subscription statuses (17 yr old = true; 21 yr old = false, etc.)
 var age = [17, 21, 25, 28, 40, 15];
 var isSubscribed = [true, false, true, false, true, true];
 
 var ageLength = age.length;
-// This counts how many items are in the array 'age'
+// This counts how many items are in the array 'age' above
 
 var myName = "Britt";
-// counter = 0 means it starts the loop from the first item in the array (meaning, 17 and true)
+// counter = 0 means it starts the loop from the first item in the array (meaning, 17 and true) (this variable is then called in [] in the conditional statements below)
 // counter < ageLength means when the variable 'counter' is less than the number of items in 'age' array, so run the loop body that many times (but not more than the # of items in the array)
 // the loop will run through the loop body the first time starting from place '0' (17, true) until it finds the correct conditional statement that applies to the items in both arrays, then will move onto the next item in the array ( counter ('0') +1)
 
@@ -23,6 +25,8 @@ for (var counter = 0; counter < ageLength; counter = counter + 1) {
     console.log("Please try again");
   }
 }
+
+// This is my stuff from last assignment but I want to keep it here for myself ok thx :)
 // if (age === 18 && isSubscribed !== true) {
 //   console.log("The user is younger than 18 and is not subscribed");
 // } else if (age < 18 && isSubscribed) {
